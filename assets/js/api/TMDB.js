ec11520aacb908311fbc56fb8637453c2b063f6b
RubensGJ <rubinho7.rr@gmail.com>
2025-02-10 17:01:02 -0400

    Carrosel part2

--- assets/js/api/TMDB.js
+++ assets/js/api/TMDB.js
@@ -1,16 +1,37 @@
 const apiKey = "30fa6e17dff8fe90ffee79a3fcaf0175";
 const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
 
-// Busca dados da API do TMDb
+function getMovieDetails(movieId) {
+  const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=pt-BR`;
+  return fetch(movieDetailsUrl).then((response) => response.json());
+}
+
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
     const carousel = $(".carousel");
     data.results.forEach((movie) => {
       const imageUrl = `https://image.tmdb.org/t/p/w780${movie.backdrop_path}`;
-      carousel.slick(
-        "slickAdd",
-        `<div><img src="${imageUrl}" alt="${movie.title}"></div>`
-      );
+      
+      // Buscar detalhes do filme
+      getMovieDetails(movie.id).then((details) => {
+        const genres = details.genres.map((genre) => genre.name).join(", ");
+        const duration = details.runtime;
+        const rating = details.vote_average;
+
+        carousel.slick(
+          "slickAdd",
+          `<div class="filme">
+            <div class="blur-background" style="background-image: url('${imageUrl}');"></div>
+            <img src="${imageUrl}" alt="${movie.title}">
+            <div class="filme-info">
+              <h3>${movie.title}</h3>
+              <p>${genres}</p>
+              <p>${duration} min</p>
+              <p> ${rating}</p>
+            </div>
+          </div>`
+        );
+      });
     });
   });

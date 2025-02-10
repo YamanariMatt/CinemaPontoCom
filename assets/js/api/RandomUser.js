60b4ee0a67d540ca7d9be629a7b74e4f12ef2b4e
RubensGJ <rubinho7.rr@gmail.com>
2025-02-10 14:15:14 -0400

    Ajustes

--- assets/js/api/RandomUser.js
+++ assets/js/api/RandomUser.js
@@ -1,30 +1,28 @@
 async function fetchUsers() {
-    const response = await fetch('https://randomuser.me/api/?results=1');
-    const data = await response.json();
-    return data.results;
+  const response = await fetch("https://randomuser.me/api/?results=1");
+  const data = await response.json();
+  return data.results;
 }
-
 
 async function displayUsers() {
-    const users = await fetchUsers();
-    const userContainer = document.getElementById('user-container');
+  const users = await fetchUsers();
+  const userContainer = document.getElementById("user-container");
 
-    users.forEach(user => {
-        const userDiv = document.createElement('div');
-        userDiv.classList.add('user');
+  users.forEach((user) => {
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user");
 
-        const userImage = document.createElement('img');
-        userImage.src = user.picture.thumbnail;
-        userImage.alt = `${user.name.first} ${user.name.last}`;
+    const userImage = document.createElement("img");
+    userImage.src = user.picture.thumbnail;
+    userImage.alt = `${user.name.first} ${user.name.last}`;
 
-        const userName = document.createElement('p');
-        userName.textContent = `${user.name.first} ${user.name.last}`;
+    const userName = document.createElement("p");
+    userName.textContent = `${user.name.first} ${user.name.last}`;
 
-        userDiv.appendChild(userImage);
-        userDiv.appendChild(userName);
-        userContainer.appendChild(userDiv);
-    });
+    userDiv.appendChild(userImage);
+    userDiv.appendChild(userName);
+    userContainer.appendChild(userDiv);
+  });
 }
-
 
 window.onload = displayUsers;

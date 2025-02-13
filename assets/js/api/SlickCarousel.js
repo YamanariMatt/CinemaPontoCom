162140cf7b4b5cd305374b963b782f0514c796e2
RubensGJ <rubinho7.rr@gmail.com>
2025-02-13 16:11:30 -0400

    adicionado novos generos

--- assets/js/api/SlickCarousel.js
+++ assets/js/api/SlickCarousel.js
@@ -1,16 +1,15 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: '20%',
-      autoplay: true,
-      autoplaySpeed: 2000,
-    });
+$(document).ready(function() {
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '20%',
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
+});
 
-  
 $(document).ready(function() {
   $('.launches-container').slick({
     infinite: true,
@@ -22,6 +21,96 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('.top-rated-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.trending-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.action-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.comedy-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.drama-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.classic-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.animation-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.horror-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.scifi-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.romance-container').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: false,
+  });
+});
+
+$(document).ready(function() {
+  $('.documentary-container').slick({
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 2,
--- assets/js/api/TMDB.js
+++ assets/js/api/TMDB.js
@@ -2,6 +2,17 @@ const apiKey = "30fa6e17dff8fe90ffee79a3fcaf0175";
 const apiUrlPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
 const apiUrlLaunches = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=pt-BR&page=`;
 const apiUrlTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=pt-BR&page=1`;
+const apiUrlTrending = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&language=pt-BR&page=1`;
+const apiUrlUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=pt-BR&page=1`;
+const apiUrlAction = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28&language=pt-BR&page=1`;
+const apiUrlComedy = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=35&language=pt-BR&page=1`;
+const apiUrlDrama = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=18&language=pt-BR&page=1`;
+const apiUrlClassic = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_date.lte=2000-01-01&language=pt-BR&page=1`;
+const apiUrlAnimation = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=16&language=pt-BR&page=1`;
+const apiUrlHorror = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27&language=pt-BR&page=1`;
+const apiUrlSciFi = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=878&language=pt-BR&page=1`;
+const apiUrlRomance = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=10749&language=pt-BR&page=1`;
+const apiUrlDocumentary = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=99&language=pt-BR&page=1`;
 
 const addedMovieIds = new Set(); // Conjunto para rastrear os filmes já adicionados
 
@@ -13,7 +24,7 @@ function getMovieDetails(movieId) {
 function addMovieToMainCarousel(carousel, movie, details, imageUrl) {
   const genres = details.genres.map((genre) => genre.name).join(" | ");
   const duration = details.runtime;
-  const rating = details.vote_average.toFixed(1); 
+  const rating = details.vote_average.toFixed(1);
 
   carousel.slick(
     "slickAdd",
@@ -32,14 +43,15 @@ function addMovieToMainCarousel(carousel, movie, details, imageUrl) {
 
 function addMovieToSecondaryCarousel(carousel, movie, details, imageUrl) {
   const duration = details.runtime;
-  const rating = details.vote_average.toFixed(1); 
+  const rating = details.vote_average.toFixed(1);
+
   carousel.slick(
     "slickAdd",
     `<div class="subfilmes">
       <img src="${imageUrl}" alt="${movie.title}">
-       <h3>${movie.title}</h3>
-        <p>${duration} min | ${rating}/10</p>
-     </div>`
+      <h3>${movie.title}</h3>
+      <p>${duration} min | ${rating}/10</p>
+    </div>`
   );
 }
 
@@ -80,12 +92,188 @@ fetch(apiUrlPopular)
 
 // Fetch multiple pages for launches
 const launchesCarousel = $(".launches-container");
-fetchMovies(apiUrlLaunches, launchesCarousel, addMovieToSecondaryCarousel, 3); // Adjust the number of pages as needed
+fetchMovies(apiUrlLaunches, launchesCarousel, addMovieToSecondaryCarousel, 3); // Ajuste o número de páginas conforme necessário
 
 fetch(apiUrlTopRated)
   .then((response) => response.json())
   .then((data) => {
     const carousel = $(".top-rated-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlTrending)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".trending-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlUpcoming)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".upcoming-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlAction)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".action-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlComedy)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".comedy-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlDrama)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".drama-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlClassic)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".classic-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlAnimation)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".animation-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlHorror)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".horror-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlSciFi)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".scifi-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlRomance)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".romance-container");
+    data.results.forEach((movie) => {
+      if (!addedMovieIds.has(movie.id)) {
+        addedMovieIds.add(movie.id);
+        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+        getMovieDetails(movie.id).then((details) => {
+          addMovieToSecondaryCarousel(carousel, movie, details, imageUrl);
+        });
+      }
+    });
+  });
+
+fetch(apiUrlDocumentary)
+  .then((response) => response.json())
+  .then((data) => {
+    const carousel = $(".documentary-container");
     data.results.forEach((movie) => {
       if (!addedMovieIds.has(movie.id)) {
         addedMovieIds.add(movie.id);

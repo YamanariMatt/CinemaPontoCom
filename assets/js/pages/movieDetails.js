fce681d46d4b63f4d40de8cf244e3d583503004d
RubensGJ <rubinho7.rr@gmail.com>
2025-02-27 10:18:58 -0400

    Availability container

--- assets/js/pages/movieDetails.js
+++ assets/js/pages/movieDetails.js
@@ -11,6 +11,11 @@ function getMovieCast(movieId) {
   return fetch(movieCastUrl).then((response) => response.json());
 }
 
+function getMoviePlatforms(movieId) {
+  const moviePlatformsUrl = `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${apiKey}`;
+  return fetch(moviePlatformsUrl).then((response) => response.json());
+}
+
 function updateRatingCircle(rating) {
   const circle = document.querySelector(".rating-circle");
   const radius = circle.r.baseVal.value;
@@ -78,7 +83,7 @@ $(document).ready(function () {
             fetch(`https://api.themoviedb.org/3/person/${director.id}/movie_credits?api_key=${apiKey}&language=pt-BR`)
               .then(response => response.json())
               .then(movieCredits => {
-                const directorMovies = movieCredits.crew.filter(movie => movie.job === "Director").slice(0, 5);
+                const directorMovies = movieCredits.crew.filter(movie => movie.job === "Director").slice(0, 6);
                 directorMovies.forEach(movie => {
                   $("#director-movies").append(`<li>${movie.title} (${new Date(movie.release_date).getFullYear()})</li>`);
                 });
@@ -86,6 +91,17 @@ $(document).ready(function () {
           });
       }
 
+      return getMoviePlatforms(movieId);
+    })
+    .then((platformData) => {
+      const platforms = platformData.results.BR.flatrate || [];
+      const platformsContainer = $("#movie-platforms");
+      platforms.forEach((platform) => {
+        const platformElement = `
+          <img class="platform-logo" src="https://image.tmdb.org/t/p/w200${platform.logo_path}" alt="${platform.provider_name}">
+        `;
+        platformsContainer.append(platformElement);
+      });
 
       $('.cast-carousel').slick({
         infinite: false,
